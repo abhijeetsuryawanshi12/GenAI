fb859d7b20fd8f8e112dfdb2b448d1c44318d0e2
abhijeetsuryawanshi12 <abhijeetsuryawanshi128@gmail.com>
2025-01-18 12:40:04 +0530

    added streamlit

--- LANGCHAIN/1-Langchain/1.2-ollama/app.py
+++ LANGCHAIN/1-Langchain/1.2-ollama/app.py
@@ -11,3 +11,25 @@ load_dotenv()
 os.environ["LANGCHAIN_API_KEY"]=os.getenv("LANGCHAIN_API_KEY")
 os.environ["LANGCHAIN_TRACING_V2"]="true"
 os.environ["LANGCHAIN_PROJECT"]=os.getenv("LANGCHAIN_PROJECT")
+
+print(os.environ["LANGCHAIN_API_KEY"])
+
+##Prompt Template
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "You are a helpful assistant. Please respond to the question asked"),
+        ("user", "Question:{question}"),
+    ]
+)
+
+## streamlit framework
+st.title("Langchain Demo With LLAMA3.2:3b Model")
+input_text=st.text_input("What question you have in mind?")
+
+##Ollama Llama2 model
+llm=Ollama(model="llama3.2:3b")
+output_parser=StrOutputParser()
+chain=prompt|llm|output_parser
+
+if input_text:
+    st.write(chain.invoke({"question":input_text}))
