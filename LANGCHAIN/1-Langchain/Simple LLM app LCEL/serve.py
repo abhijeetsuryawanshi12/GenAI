d2042641ffc620906423c870b451e8fdecd1e42d
abhijeetsuryawanshi12 <abhijeetsuryawanshi128@gmail.com>
2025-01-19 11:50:32 +0530

    created app and used fastapi

---
+++ LANGCHAIN/1-Langchain/Simple LLM app LCEL/serve.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+from langchain_groq import ChatGroq
+import os
+from langserve import add_routes
+from dotenv import load_dotenv
+load_dotenv()
+
+groq_api_key=os.getenv("GROQ_API_KEY")
+model=ChatGroq(model="Gemma2-9b-It",groq_api_key=groq_api_key)
+
+system_template = "Translate ythe following into {language}:"
+prompt_template = ChatPromptTemplate.from_messages([
+    ("system", system_template),
+    ("user", '{text}')
+])    
+
+parser=StrOutputParser()
+
+chain=prompt_template|model|parser
+
+app=FastAPI(title="Langchain Server",
+            version="1.0",
+            description="A simple API server using Langchain runnnable interfaces")
+
+add_routes(app, chain, path='/chain')
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="localhost", port=8000)
